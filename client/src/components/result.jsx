d8c86748b4d241f525662dd4ee7ef72fe1baef44
gusalviarez <gusalv2001@gmail.com>
2024-06-05 11:25:54 -0400

    add screenshot

--- client/src/components/result.jsx
+++ client/src/components/result.jsx
@@ -1,10 +1,12 @@
-
-import { Link } from 'react-router-dom';
 import { CircularProgressbar } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
-import Screenshot from './screenshot';
+import screenShot from '../../public/screen.png';
+
 const Result = () => {
   const percentage = 30;
+  const percentage2 = 100;
+  const percentage3 = 70;
+  const percentage4 = 100;
 
   return (
     <div className='result-container'>
@@ -15,20 +17,26 @@ const Result = () => {
           <span>Performance</span>
         </a>
         <a className='progress-container' href='./'>
-          <CircularProgressbar value={percentage} text={`${percentage}%`} />
+          <CircularProgressbar value={percentage2} text={`${percentage2}%`} />
           <span>Accesibility</span>
         </a>
         <a className='progress-container' href='./'>
-          <CircularProgressbar value={percentage} text={`${percentage}%`} />
+          <CircularProgressbar value={percentage3} text={`${percentage3}%`} />
           <span>Best practices</span>
 
         </a>
         <a className='progress-container' href='./'>
-          <CircularProgressbar value={percentage} text={`${percentage}%`} />
+          <CircularProgressbar value={percentage4} text={`${percentage4}%`} />
           <span>SEO</span>
         </a>
       </div>
-      <Screenshot />
+      <div className='performance-container'>
+        <div>
+          <h2 className='performance-title'>Performance</h2>
+          <CircularProgressbar value={percentage} text={`${percentage}%`} />
+        </div>
+        <img className='screenshot-img' alt="screenShot of the url page" src={screenShot} />
+      </div>
     </div>
   );
 }
